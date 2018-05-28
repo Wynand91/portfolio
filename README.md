5b1a793eacb78933d0d38a71dd25cbe8b4bca8b5
wynand <wtheron2@yahoo.com>
2018-05-28 15:04:49 +0200

    added sticky navbar and paralax scrolling for jumbotron

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portfolio
 my portfolio page
+
+Author: Wynand Theron
+Email: wtheron2@yahoo.com
+
+Please note, the reason I alternate between using standard JavaScript and jQuery is just to indicate that I am familiar with both.  
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+/* ******************
+Author: Wynand Theron
+email: wtheron2@yahoo.//
 
+Please note, the reason I use both standard JavaScript and jQuery is just to indicate
+that I am familiar with both. 
+****************** */
+
+$(document).ready(function(){
+
+  $(window).on("scroll", function() {
+    if ($(window).scrollTop()) {
+      $('nav').addClass('black')
+    }
+    else {
+      $('nav').removeClass('black');
+    }
+  })
+
+});
