dbcca0a50e7ac74e8dadb6067c56f08bd178f477
wynand <wtheron2@yahoo.com>
2018-06-01 15:58:52 +0200

    Added 'contact' section and fixed internet explorer compatability issues

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ Author: Wynand Theron
 email: wtheron2@yahoo.//
 
 Please note, the reason I use both standard JavaScript and jQuery is just to indicate
-that I am familiar with both. 
+that I am familiar with both.
 ****************** */
 
 $(document).ready(function(){
 
   $(window).on("scroll", function() {
     if ($(window).scrollTop()) {
-      $('nav').addClass('black')
+      $('.nav').addClass('black');
+      $(".nav #logo a").css("color","#fff");
     }
     else {
-      $('nav').removeClass('black');
+      $('.nav').removeClass('black');
+      $(".nav #logo a").css("color","#333");
     }
   })
 
