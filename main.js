14e62e9e845c6e31c58e6e6d71990c587de9d036
wynand <wtheron2@yahoo.com>
2018-06-04 17:20:31 +0200

    Made website responsive for tablets and large phones

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ that I am familiar with both.
 
 $(document).ready(function(){
 
+//For the nav bar color change when scrolling
   $(window).on("scroll", function() {
     if ($(window).scrollTop()) {
       $('.nav').addClass('black');
@@ -16,6 +17,17 @@ $(document).ready(function(){
     else {
       $('.nav').removeClass('black');
       $(".nav #logo a").css("color","#333");
+    }
+  });
+
+//burger-nav open/close
+  $("#burger-nav").on("click", function(){
+    $(".nav ul").toggleClass("open");
+  });
+
+  $(".nav li a").on("click", function(){
+    if ($(".nav ul").hasClass("open")) {
+      $(".nav ul").removeClass("open");
     }
   })
 
